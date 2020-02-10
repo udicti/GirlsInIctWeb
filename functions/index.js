ea0f02116cf5d78a3a4de557b0980a881ccc0808
joshua <mama27j@gmail.com>
2020-02-10 09:44:15 +0300

    add bfast functions

---
+++ functions/index.js
@@ -0,0 +1,9 @@
+// Create and Deploy Your First BFast Function
+
+
+// exports.helloWorld = {
+//     path: '/helloWorld',
+//     onRequest: (request, response) => {
+//         response.send("Hello from BFast!");
+//     }
+// };
