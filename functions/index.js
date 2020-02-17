501255480f7739fcc3ebca5103d8ad826b2068e7
joshua <mama27j@gmail.com>
2020-02-17 23:24:09 +0300

    initial

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 // Create and Deploy Your First BFast Function
 
 
-// exports.helloWorld = {
-//     path: '/helloWorld',
-//     onRequest: (request, response) => {
-//         response.send("Hello from BFast!");
-//     }
-// };
+exports.helloWorld = {
+    path: '/hello',
+    onRequest: (request, response) => {
+        response.send("Hello from SmartGirls!");
+    }
+};
