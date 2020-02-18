c76268e3f582835f17575a966d071db9c9e5192b
joshua <mama27j@gmail.com>
2020-02-18 18:31:21 +0300

    add footer and update nav bar

--- functions/router/home.js
+++ functions/router/home.js
@@ -8,15 +8,15 @@ homeRouter.get('/', (request, response) => {
         <html lang="en">
            <head>
                <title>SmartGirl</title>
-               <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-               <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap" rel="stylesheet">
+               <link rel="stylesheet" href="/bootstrap4.css">
+               <link href="/roboto.css" rel="stylesheet">
                <link rel="stylesheet" href="/style.css">
            </head>
            <body>
                 ${Landing()}
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+                <script src="/js/jquery.js"></script>
+                <script src="/js/popper.js"></script>
+                <script src="/js/bootstrap.js"></script>
             </body>
         </html>
     `)
